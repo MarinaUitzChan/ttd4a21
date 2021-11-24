9964753dc496874d092f89da546360cd5ab07ed6
MarinaUitzChan <marinauitzchan@gmail.com>
2021-11-24 12:15:32 -0600

    Video Punto de Venta
    PV Presentando Productos en TABLA

--- resources/views/ventas.blade.php
+++ resources/views/ventas.blade.php
@@ -8,7 +8,7 @@
 			<div class="col-md-4"><!--INICIO DE COL-->
 				
 				<div class="input-group mb-3">
-					<input type="text" class="form-control" placeholder="Escriba el codigo de producto" arial-describedby="basic-addon2" v-model="sku">
+					<input type="text" class="form-control" placeholder="Escriba el codigo de producto" arial-describedby="basic-addon2" v-model="sku" v-on:keyup.enter="buscarProducto()">
 
 					<div class="input-group-append">
 						<button class="btn btn-outline-secondary" type="button" @click="buscarProducto()">Buscar</button>
@@ -19,8 +19,31 @@
 			</div><!--FIN DE COL-->	
 		</div><!--FIN DE ROW-->
 
-		@{{ventas}}
+		<div class="row">
+			<div class="col-md-12">
+				<table class="table table-bordered">
+					<thead>
+						<th>SKU</th>
+						<th>PRODUCTO</th>
+						<th>PRECIO</th>
+						<th>CANTIDAD</th>
+						<th>TOTAL</th>
+					</thead>
 
+					<tbody>
+						<tr v-for="venta in ventas">
+							<td>@{{venta.sku}}</td>
+							<td>@{{venta.nombre}}</td>
+							<td>@{{venta.precio}}</td>
+							<td>@{{venta.cantidad}}</td>
+							<td>@{{venta.total}}</td>
+						</tr>
+					</tbody>
+				</table>
+				
+			</div>
+			
+		</div>
 	</div><!--FIN DE CONTAINER-->
 	
 </div><!--FIN DE VUE-->
